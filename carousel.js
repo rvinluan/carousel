e53290d5e97f28fed75efeada4f00643b08436e1
Robert Vinluan <robert.vinluan@nytimes.com>
2015-12-10 00:10:05 -0500

    layout without touching

--- carousel.js
+++ carousel.js
@@ -1,11 +1,14 @@
 var canvas = $("#screen"),
     c = canvas[0].getContext('2d');
 
-var Baggage = function() {
+var Baggage = function(xC, yC, col) {
+    this.x = xC;
+    this.y = yC;
     this.width = this.generateProperty("width");
     this.height = this.generateProperty("surfaceArea") / this.width;
     this.depth = this.generateProperty("depth");
     this.radius = this.generateProperty("radius");
+    this.id = col;
 }
 
 Baggage.prototype.CONSTRAINTS = {
@@ -24,19 +27,82 @@ Baggage.prototype.generateProperty = function(propName) {
     return Math.floor(Math.random() * (max - min) + min);
 }
 
-Baggage.prototype.render = function(x, y) {
+Baggage.prototype.render = function() {
     c.fillStyle = "#999999";
-    roundRect(c, x, y, this.width, this.height, this.radius, c.fillStyle, false);
+    roundRect(c, this.x, this.y, this.width, this.height, this.radius, c.fillStyle, false);
     c.fillStyle = "#AAAAAA";
-    roundRect(c, x, y + this.depth, this.width, this.height - this.depth, this.radius, c.fillStyle, false);
+    roundRect(c, this.x, this.y + this.depth, this.width, this.height - this.depth, this.radius, c.fillStyle, false);
+    // c.fillStyle = "#000000";
+    // c.fillText(this.id, this.x, this.y);
+}
+
+function dlb(bag) {
+    var ty = bag.y + bag.height + 30;
+    c.beginPath();
+    c.moveTo(bag.x, ty);
+    c.lineTo(bag.x + bag.width, ty);
+    c.strokeStyle = "#FF0000"
+    c.stroke();
+}
+
+Baggage.prototype.collapse = function(lastRow, thisIndex) {
+    if(lastRow.length == 0) {
+        return;
+    }
+    var targetBaggage = lastRow[thisIndex],
+        targetY;
+    if(targetBaggage) {
+        targetY = targetBaggage.y + targetBaggage.height + 30;
+        //debug
+        //go left and try to find an intersecting bag
+        for(var i = thisIndex - 1; i >= 0; i--) {
+            if(lastRow[i]) {
+                var testX = lastRow[i].x + lastRow[i].width + 30;
+                if(testX < this.x) {
+                    break;
+                } else if(lastRow[i].y + lastRow[i].height + 30 > targetY) {
+                    targetY = lastRow[i].y + lastRow[i].height + 30;
+                    break;
+                } else {
+                    break;
+                }
+            }
+        }
+        //go right and try to find an intersecting bag
+        for(var i = thisIndex + 1; i < lastRow.length; i++) {
+            if(lastRow[i]) {
+                var testX = lastRow[i].x;
+                if(testX > this.x + this.width + 30) {
+                    break;
+                } else if(lastRow[i].y + lastRow[i].height + 30 > targetY) {
+                    targetY = lastRow[i].y + lastRow[i].height + 30;
+                }
+            }
+        }
+        this.y = targetY;
+    }
 }
 
 var baggages = [],
+    lastRow = [],
+    thisRow = [],
     lastLeft = 25;
+    row = 0,
+    col = 0;
 
-for(var i = 0; i < 5; i++) {
-    var b = new Baggage();
+for(var i = 0; i < 30; i++) {
+    var b = new Baggage(lastLeft, row*200 + Math.floor(Math.random() * (75 + 25) - 25), col);
+    b.collapse(lastRow, col);
     baggages.push(b);
-    b.render(lastLeft, 200 + Math.floor(Math.random() * (75 + 25) - 25));
+    thisRow.push(b);
+    b.render();
     lastLeft = lastLeft + b.width + 30;
+    col++;
+    if(lastLeft >= 800) {
+        lastLeft = 0;
+        col = 0;
+        row++;
+        lastRow = thisRow;
+        thisRow = [];
+    }
 }
